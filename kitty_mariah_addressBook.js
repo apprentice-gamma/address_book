454d29bae7c58ebae8ba986cce8660bd0ad0cecd
KittyGarrison <[katrina.garrison@detroitlabs.com]>
2015-03-11 17:22:18 -0400

    new js file with object constructors

---
+++ kitty_mariah_addressBook.js
@@ -0,0 +1,83 @@
+var sget = require("sget");
+
+
+//Object constructors
+
+function AddressBook (entries){
+
+	this.entries = entries;
+
+
+	this.addEntry = function(){
+	//push to addyBook
+	};
+
+
+	this.deleteEntry = function(){
+	//using the searchEntries function get index and use addyBook.splice([index],1) to remove entry.
+	};
+
+
+	this.listEntries = function(){
+	console.log()
+	};
+
+
+	this.searchEntries = function(){
+	//use findIndex() to return index of entry, plug into display function
+	};
+
+	this.displayEntry = function(){
+	// use index of addyBook array to display fields of entry
+	};
+
+	this.userMenu = function(){
+		var userMenuChoice = sget("Welcome\n What would you like to do with your address book?\n Enter number or exit\n 1- list entries\n 2- search for entry\n 3- add entry\n 4- delete entry\n exit- to leave\n").trim();
+
+		switch (userMenuChoice){
+			case "1":
+				console.log("OK, list entries.");
+				break;
+			case "2":
+				console.log("OK, search for entry."); 
+				break;
+			case "3":
+				console.log("OK, add entry."); 				 
+				break;
+			case "4":
+				console.log("OK, delete entry."); 				 
+				break;	
+			case "exit":
+				console.log("Goodbye.");
+				break;
+			default:
+				console.log("Invalid request. Please try again.");
+				return this.userMenu();
+				break;
+		};
+	}
+}; 
+
+function Entry (name, address, phoneNumber){
+	this.name = name;
+	this.address = address;
+	this.phoneNumber = phoneNumber;
+};
+
+
+//New objects
+
+
+var firstBook = new AddressBook([Mariah, Kitty, Erika, Robb]);
+
+var Mariah = new Entry("Mariah", "France", "313-555-6666");
+var Kitty = new Entry("Kitty", "Jamaica", "313-555-8888");
+var Erika = new Entry("Erika", "Ecuador", "313-555-5555" );
+var Robb = new Entry ("Robb", "Antarctica", "313-555-7777");
+
+//Running program
+
+console.log("\n-----------------------------------------------\n Welcome to your Address Book!\n You currently have 4 entries in your book,\n feel free to add, delete, list, or search the entries!\n-----------------------------------------------\n");
+
+firstBook.userMenu();
+
