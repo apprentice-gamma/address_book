eb7aece70d8196a627a3c59f96d4974fb4ddfd63
Nikki Evelyn <nikki.evelyn1@gmail.com>
2015-03-11 13:31:16 -0400

    Setting up Objects

--- nikki_marc_addressbook.js
+++ nikki_marc_addressbook.js
@@ -1,1 +1,22 @@
-//nikki and marc
+/*Address Book assignment for Apprentice Gamma
+
+1.) Create an address book which contains at least four entries.
+Each entry should contain a person’s name, address, and phone number
+
+2.) Your program should allow the user to: 
+a.) add entries to the address book 
+b.) delete entries from the address book 
+c.) list all entries in the address book 
+d.) search for an entry in the address book*/
+
+
+function Contact(name, address, phoneNumber){
+	this.name= name;
+	this.address= adress;
+	this.phoneNumber= phoneNumber;
+	this.addNewContact= function(){};
+	this.deleteContact = function (){};
+	this.searchContacts= function(){};
+	this.listContacts=function (){};
+
+	
