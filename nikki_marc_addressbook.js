71cfba2cb4e2ec58cefac0970d3fb7c5c5d0e0e2
Marc Nischan <marcnischan@gmail.com>
2015-03-11 13:28:33 -0400

    adding new contact objects

--- nikki_marc_addressbook.js
+++ nikki_marc_addressbook.js
@@ -1,1 +1,6 @@
 //nikki and marc
+
+var Marc = new Contact("Marc", "3675 Wakefield Rd.", "248 709-0160");
+var Nikki = new Contact("Marc", "1234 Anystreet", "313 555-1212");
+var Batman = new Contact("Bruce", "The Batcave", "313 BAT-CAVE");
+var Paul = new Contact("Paul", "1520 Woodward Ave.", "313 473-0607");
