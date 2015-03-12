1fcf02adfb501d7348717b5ff0ad43e091a378ac
Kelsey Ando <kma3a@virginia.edu>
2015-03-11 22:01:10 -0400

    adding the add, delete, and list methods to address book

--- addressBook.js
+++ addressBook.js
@@ -10,12 +10,26 @@ function Contact(name, address, phone) {
 }
 
 Contact.prototype.view = function(){
-	console.log(this.name + " lives at " + this.address + " and phone number is " + this.phone + ".");
+	return console.log(this.name + " lives at " + this.address + " and phone number is " + this.phone + ".");
 };
 
 function AddressBook(contacts) {
 	this.contacts = contacts;
+	this.list = function() {
+		this.contacts.forEach(function(contact) {
+			contact.view();
+		})
+	};
 }
+
+AddressBook.prototype.add = function(contact) {
+	this.contacts.push(contact);
+};
+
+AddressBook.prototype.delete = function(contact) {
+	var index = this.contacts.indexOf(contact);
+	this.contacts.splice(index, 1);
+};
 
 var bozo = new Contact("Bozo the Clown", "Circus", "333-543-4567");
 var dog = new Contact("Spot the Dog", "The Doghouse", "123-456-7890");
@@ -24,4 +38,7 @@ var john = new Contact("John Doe", "Alaska", "999-999-2302");
 
 myAddressBook = new AddressBook([bozo,dog,jane,john]);
 
-console.log(myAddressBook.contacts);
+var matt = new Contact("Matt Doe", "LA", "888-909-9000");
+console.log("-----------------------")
+myAddressBook.delete(dog);
+myAddressBook.list();
